dc8c0d9d9e221d404dc105e7b064f02193878cbd
Howard Lovatt <howard.lovatt@gmail.com>
2022-01-13 17:31:13 +1100

    "Add files specific to WiPy port."

--- pyi.py
+++ pyi.py
@@ -11,7 +11,7 @@ from class_ import Class, strip_leading_and_trailing_blank_lines
 __author__ = rst.__author__
 __copyright_ = rst.__copyright__
 __license__ = rst.__license__
-__version__ = "7.5.0"  # Version set by https://github.com/hlovatt/tag2ver
+__version__ = "7.5.2"  # Version set by https://github.com/hlovatt/tag2ver
 
 
 @dataclass(frozen=True)
